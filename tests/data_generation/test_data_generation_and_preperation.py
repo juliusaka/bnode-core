7f39756f006758f681954314ca3cd7c59777fa7d
Julius Aka <aka.julius@hotmail.de>
2025-10-24 12:30:09 +0200

    add tests for raw data generation and data preparation

---
+++ tests/data_generation/test_data_generation_and_preperation.py
@@ -0,0 +1,42 @@
+import sys
+import hydra
+from bnode_core import filepaths 
+from pathlib import Path
+from bnode_core.data_generation.raw_data_generation import  main
+from bnode_core.config import get_config_store
+import shutil
+from hydra.core.global_hydra import GlobalHydra
+
+def test_01_raw_data_generation():
+    cs = get_config_store()
+    # avoid passing pytest's CLI args into the called main()
+    orig_argv = sys.argv[:]
+    try:
+        sys.argv = [orig_argv[0], '--config-dir=resources/config',
+                    '--config-name=data_generation',
+                    'pModel.RawData.n_samples=12',
+                    'pModel.RawData.versionName=test',
+                    ]
+        hydra.initialize_config_dir(config_dir=str(Path('resources/config').absolute()), version_base=None)
+        cfg = hydra.compose(config_name='data_generation', overrides=sys.argv[3:])
+        if filepaths.dir_raw_data(cfg).exists():
+            shutil.rmtree(filepaths.dir_raw_data(cfg))
+        # correctly clear Hydra global state
+        GlobalHydra.instance().clear()
+        main()
+    finally:
+        sys.argv = orig_argv
+
+def test_02_data_preperation():
+    from bnode_core.data_generation.data_preperation import main
+    import sys
+    # avoid passing pytest's CLI args into the called main()
+    orig_argv = sys.argv[:]
+    try:
+        sys.argv = [orig_argv[0], '--config-dir=resources/config',
+                    '--config-name=data_generation',
+                    'pModel.RawData.versionName=test',
+                    ]
+        main()
+    finally:
+        sys.argv = orig_argv
