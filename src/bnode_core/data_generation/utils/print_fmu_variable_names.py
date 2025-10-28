42722ea09bab628dbc3d1e65f3e916edc4b8356b
Julius Aka <julius.aka@uni-a.de>
2025-10-28 10:40:44 +0100

    bugfix in print fmu_variable_names

--- src/bnode_core/data_generation/utils/print_fmu_variable_names.py
+++ src/bnode_core/data_generation/utils/print_fmu_variable_names.py
@@ -87,7 +87,7 @@ def print_variable_names(fmu_path: Path):
         print(name + ':')
     print('\nThe outputs are:')
     for name in get_output_names(fmu_path):
-        print(name + ':')
+        print('-' + name)
     print('\nThe variables are:')
     for name in get_variable_names(fmu_path):
         print('- ' + name)
@@ -99,18 +99,19 @@ def main():
     if '--help' in sys.argv or '-h' in sys.argv:
         print('Usage: python print_fmu_variable_names.py [--cfg_path=path_to_config.yaml | --fmu_path=path_to_fmu]')
         print('If both is not provided, the script will try to auto-recognize the config file location.')
+        print('Hint: Filepaths need to be provided using forward slashes (/) on Windows.')
         sys.exit(0)
     
     cfg_path = None
     fmu_path = None
     for i, arg in enumerate(sys.argv):
-        if arg.startswith('--cfg_path'):
+        if arg.startswith('--cfg_path') or arg.startswith('--cfg-path'):
             cfg_path = sys.argv.pop(i).split('=')[1]
-        if arg.startswith('--fmu_path'):
+        if arg.startswith('--fmu_path') or arg.startswith('--fmu-path'):
             fmu_path = sys.argv.pop(i).split('=')[1]
     if cfg_path is not None and fmu_path is not None:
         raise ValueError('Please provide either --cfg_path or --fmu_path, not both.')
-    
+
     if fmu_path is not None:
         fmu_path = Path(fmu_path)
     else:
