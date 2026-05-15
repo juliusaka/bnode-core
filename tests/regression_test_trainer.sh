b98d11b159ea1cf9bc79b2eb9f5d4bfe66e424f5
Julius Aka (WSL, Ubuntu_SSD) <julius.aka@uni-a.de>
2026-05-15 14:03:22 +0200

    updated patience

--- tests/regression_test_trainer.sh
+++ tests/regression_test_trainer.sh
@@ -69,7 +69,8 @@ run_training() {
             "mlflow_experiment_name=regression" \
             "mlflow_run_name=$run_name" \
             "mlflow_tracking_uri=file://$MLRUNS_DIR" \
-            "nn_model.training.max_epochs_override=1000"
+            "nn_model.training.max_epochs_override=10000" \
+            "nn_model.training.early_stopping_patience=800" \
     )
 }
 
